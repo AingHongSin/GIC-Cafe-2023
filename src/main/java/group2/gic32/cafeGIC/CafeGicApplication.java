8f78520808359a72ce96be7bd0e433c7973e2c3f
Hongsin <ainghongsin9999@gmail.com>
2023-05-22 14:19:55 +0700

    Create java project with sprinboot framwork

---
+++ src/main/java/group2/gic32/cafeGIC/CafeGicApplication.java
@@ -0,0 +1,13 @@
+package group2.gic32.cafeGIC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeGicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeGicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group2/gic32/cafeGIC/CafeGicApplicationTests.java
@@ -0,0 +1,13 @@
+package group2.gic32.cafeGIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeGicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
