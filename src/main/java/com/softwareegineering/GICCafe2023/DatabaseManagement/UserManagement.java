9f258f317a508aeba5ac3610e85a3d0406ec01dd
Hongsin <ainghongsin9999@gmail.com>
2023-05-29 11:02:23 +0700

    Update User.java cahsiermanagement.html UserManagement.java cahsier.css

--- src/main/java/com/softwareegineering/GICCafe2023/DatabaseManagement/UserManagement.java
+++ src/main/java/com/softwareegineering/GICCafe2023/DatabaseManagement/UserManagement.java
@@ -11,52 +11,33 @@ public class UserManagement {
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "";
 
-    public static void addUser(User user) {
-        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "INSERT INTO user (id, first_name, last_name, sex, dob, age, username, password) " +
-                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-            try (PreparedStatement stmt = conn.prepareStatement(query)) {
-                stmt.setInt(1, user.getId());
-                stmt.setString(2, user.getFirstName());
-                stmt.setString(3, user.getLastName());
-                stmt.setString(4, user.getSex());
-                stmt.setDate(5, new java.sql.Date(user.getDob().getTime()));
-                stmt.setInt(6, user.getAge());
-                stmt.setString(7, user.getUsername());
-                stmt.setString(8, user.getPassword());
-                stmt.executeUpdate();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static List<User> getAllUsers() {
+    public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
-        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "SELECT * FROM user";
-            try (Statement stmt = conn.createStatement()) {
-                try (ResultSet rs = stmt.executeQuery(query)) {
-                    while (rs.next()) {
-                        User user = new User();
-                        user.setId(rs.getInt("id"));
-                        user.setFirstName(rs.getString("first_name"));
-                        user.setLastName(rs.getString("last_name"));
-                        user.setSex(rs.getString("sex"));
-                        user.setDob(rs.getDate("dob"));
-                        user.setAge(rs.getInt("age"));
-                        user.setUsername(rs.getString("username"));
-                        user.setPassword(rs.getString("password"));
-                        userList.add(user);
-                    }
-                }
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM user")) {
+
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setFirstName(rs.getString("first_name"));
+                user.setLastName(rs.getString("last_name"));
+                user.setSex(rs.getString("sex"));
+                user.setRole(rs.getString("role"));
+                user.setDob(rs.getDate("dob"));
+                user.setAge(rs.getInt("age"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+
+                userList.add(user);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return userList;
     }
-
 
     public static boolean loginUser(String username, String password) {
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
@@ -77,18 +58,73 @@ public class UserManagement {
         return false; // User not found or invalid credentials
     }
 
-    public static void updateUser(User user) {
-        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            String query = "UPDATE user SET first_name = ?, last_name = ?, sex = ?, dob = ?, age = ? WHERE id = ?";
-            try (PreparedStatement stmt = conn.prepareStatement(query)) {
-                stmt.setString(1, user.getFirstName());
-                stmt.setString(2, user.getLastName());
-                stmt.setString(3, user.getSex());
-                stmt.setDate(4, new java.sql.Date(user.getDob().getTime()));
-                stmt.setInt(5, user.getAge());
-                stmt.setInt(6, user.getId());
-                stmt.executeUpdate();
+    public User getUserById(int id) {
+        User user = null;
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM user WHERE id = ?")) {
+
+            stmt.setInt(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setFirstName(rs.getString("first_name"));
+                    user.setLastName(rs.getString("last_name"));
+                    user.setSex(rs.getString("sex"));
+                    user.setRole(rs.getString("role"));
+                    user.setDob(rs.getDate("dob"));
+                    user.setAge(rs.getInt("age"));
+                    user.setUsername(rs.getString("username"));
+                    user.setPassword(rs.getString("password"));
+                }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public void addUser(User user) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(
+                     "INSERT INTO user (first_name, last_name, sex, role, dob, age, username, password) " +
+                             "VALUES (?, ?, ?, ?, ?, ?, ?, ?)")) {
+
+            stmt.setString(1, user.getFirstName());
+            stmt.setString(2, user.getLastName());
+            stmt.setString(3, user.getSex());
+            stmt.setString(4, user.getRole());
+            stmt.setDate(5, new java.sql.Date(user.getDob().getTime()));
+            stmt.setInt(6, user.getAge());
+            stmt.setString(7, user.getUsername());
+            stmt.setString(8, user.getPassword());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(User user) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(
+                     "UPDATE user SET first_name = ?, last_name = ?, sex = ?, role = ?, " +
+                             "dob = ?, age = ?, username = ?, password = ? WHERE id = ?")) {
+
+            stmt.setString(1, user.getFirstName());
+            stmt.setString(2, user.getLastName());
+            stmt.setString(3, user.getSex());
+            stmt.setString(4, user.getRole());
+            stmt.setDate(5, new java.sql.Date(user.getDob().getTime()));
+            stmt.setInt(6, user.getAge());
+            stmt.setString(7, user.getUsername());
+            stmt.setString(8, user.getPassword());
+            stmt.setInt(9, user.getId());
+
+            stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/com/softwareegineering/GICCafe2023/Model/User.java
+++ src/main/java/com/softwareegineering/GICCafe2023/Model/User.java
@@ -1,110 +1,75 @@
 package com.softwareegineering.GICCafe2023.Model;
 
 import java.util.Date;
-import java.util.Map;
 
 public class User {
     private int id;
     private String firstName;
     private String lastName;
     private String sex;
+    private String role;
     private Date dob;
     private int age;
     private String username;
     private String password;
-    private Map<String, Object> attributes;
 
-    public User() {
-        
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public User(int id, String firstName, String lastName, String sex, Date dob, int age, String username,
-            String password, Map<String, Object> attributes) {
+    public User(int id, String firstName, String lastName, String sex, String role, Date dob, int age, String username,
+            String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.sex = sex;
+        this.role = role;
         this.dob = dob;
         this.age = age;
         this.username = username;
         this.password = password;
-        this.attributes = attributes;
+    }
+
+    public User() {
+        // Default constructor
     }
 
     // Getters and Setters
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getId() { return id; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public void setId(int id) { this.id = id; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public String getFirstName() {  return firstName; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public String getLastName() { return lastName; }
 
-    public String getSex() {
-        return sex;
-    }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
+    public String getSex() { return sex; }
 
-    public Date getDob() {
-        return dob;
-    }
+    public void setSex(String sex) { this.sex = sex; }
 
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
+    public String getRole() { return role; }
 
-    public int getAge() {
-        return age;
-    }
+    public void setRole(String role) { this.role = role; }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    public Date getDob() { return dob; }
 
-    public String getUsername() {
-        return username;
-    }
+    public void setDob(Date dob) { this.dob = dob; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public int getAge() { return age; }
 
-    public String getPassword() {
-        return password;
-    }
+    public void setAge(int age) { this.age = age; }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getUsername() {  return username; }
 
+    public void setUsername(String username) { this.username = username; }
 
-    // Method to add attributes
-    public void addAttribute(String attributeName, Object attributeValue) {
-        attributes.put(attributeName, attributeValue);
-    }
+    public String getPassword() { return password;}
 
-    // Method to get attributes
-    public Object getAttribute(String attributeName) {
-        return attributes.get(attributeName);
-    }
+    public void setPassword(String password) { this.password = password; }
 }
-
